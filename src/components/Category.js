b47cdafa658baf3bd1af19f279d9b52689cda8ae
kjlee <2_kj@daum.net>
2023-07-08 20:03:31 +0900

    카테고리 오타 수정

--- src/components/Category.js
+++ src/components/Category.js
@@ -42,10 +42,10 @@ export default Category
 
 const Container = styled.div`
     margin-top: 30px;
-    padding:30px 0px 26px;
+    padding: 30px 0px 26px;
     display:grid;
-    gap:25px;
-    grid-template-columns: repeat(5, 1ft);
+    gap: 25px;
+    grid-template-columns: repeat(5, 1fr);
 
     @media (max-width: 768px) {
         grid-template-columns: repeat(1, 1fr);
