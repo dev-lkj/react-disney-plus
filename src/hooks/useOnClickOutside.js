f1e3045f2edbb2fbba9805bcf10ac105efa95798
kjlee <2_kj@daum.net>
2023-07-09 10:07:42 +0900

    useDebounce, useRef 추가

---
+++ src/hooks/useOnClickOutside.js
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+export default function useOnClickOutside(ref, handler) {
+    useEffect(() => {
+     return () => {
+        
+     }
+    }, [ref, handler])
+    
+}
